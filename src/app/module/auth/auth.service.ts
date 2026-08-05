ea583e54809bea2c71502dd7c8630d7df2131930
shohagnawaz <snawazdigital@gmail.com>
2026-08-05 18:04:05 +0600

    login

---
+++ src/app/module/auth/auth.service.ts
@@ -0,0 +1,68 @@
+import { User, UserStatus } from "../../../generated/prisma/client";
+import { auth } from "../../lib/auth";
+import { prisma } from "../../lib/prisma";
+
+interface IRegisterPatientPayload {
+    name: string,
+    email: string,
+    password: string
+}
+
+const registerPatient = async (payload : IRegisterPatientPayload) => {
+    const {name, email, password} = payload;
+
+    const data = await auth.api.signUpEmail({
+        body: {
+            name,
+            email,
+            password,
+            // default values
+            // needsPasswordChange: false,
+            // role: Role.PATIENT
+        }
+    }) 
+
+    if(!data.user) {
+        throw new Error("Failed to register patient")
+    }
+
+    //TODO: create patient profile in transaction after sign up of 
+    // patient in user model
+
+    // const patient = await prisma.$transaction(async (tx) => {
+
+    // })
+
+    return data
+}
+
+interface ILogingUserPayload {
+    email: string,
+    password: string
+}
+
+const loginUser = async (payload: ILogingUserPayload) => {
+    const { email, password } = payload;
+
+    const data = await auth.api.signInEmail({
+        body: {
+            email,
+            password
+        }
+    })
+
+    if (data.user.status === UserStatus.BLOCKED) {
+        throw new Error("User is blocked");
+    }
+
+    if (data.user.isDeleted || data.user.status === UserStatus.DELETED) {
+        throw new Error("User is deleted")
+    }
+
+    return data;
+}
+
+export const AuthService = {
+    registerPatient,
+    loginUser
+};
--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { SpecialtyRoutes } from "../module/specialty/specialty.route";
+import { AuthRoutes } from "../module/auth/auth.route";
 
 const router = Router();
 
+router.use("/auth", AuthRoutes)
 router.use("/specialties", SpecialtyRoutes)
 
 export const IndexRoutes = router;
