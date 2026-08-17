c0dbda5701dfbd8a3280e9c14ebe8dd0d7ca3b3e
shohagnawaz <snawazdigital@gmail.com>
2026-08-17 19:03:12 +0600

    configEnv

--- src/config/env.ts
+++ src/config/env.ts
@@ -19,7 +19,11 @@ const loadEnvVariables = () : EnvConfig => {
         "PORT",
         "DATABASE_URL",
         "BETTER_AUTH_SECRET",
-        "BETTER_AUTH_URL"
+        "BETTER_AUTH_URL",
+        "ACCESS_TOKEN_SECRET",
+        "REFRESH_TOKEN_SECRET",
+        "ACCESS_TOKEN_EXPIRES_IN",
+        "REFRESH_TOKEN_EXPIRES_IN"
     ]
 
     requireEnvVariable.forEach((variable) => {
@@ -35,7 +39,11 @@ const loadEnvVariables = () : EnvConfig => {
         PORT: process.env.PORT as string,
         DATABASE_URL: process.env.DATABASE_URL as string,
         BETTER_AUTH_SECRET: process.env.BETTER_AUTH_SECRET as string,
-        BETTER_AUTH_URL: process.env.BETTER_AUTH_URL as string
+        BETTER_AUTH_URL: process.env.BETTER_AUTH_URL as string,
+        ACCESS_TOKEN_SECRET: process.env.ACCESS_TOKEN_SECRET as string,
+        REFRESH_TOKEN_SECRET: process.env.REFRESH_TOKEN_SECRET as string,
+        ACCESS_TOKEN_EXPIRES_IN: ,
+        "REFRESH_TOKEN_EXPIRES_IN"
     }
 };
 
